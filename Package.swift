38b5c09cdb932ab695d4a6f689b341eb19326eed
Eilon Krauthammer <mitleyber@gmail.com>
2021-03-14 12:42:58 +0200

    Cleanups

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .target(
             name: "EKSwiftSuite",
             dependencies: [],
-            resources: [.process("Resources")]
+            resources: [.copy("Resources")]
         ),
         .testTarget(
             name: "EKSwiftSuiteTests",
