39d195f56cafde4970aa3785b83523390718c40b
Eilon Krauthammer <eilon@socialkit.com>
2021-03-17 23:25:35 +0200

    Added SheetViewController

---
+++ Sources/EKSwiftSuite/UI/SheetViewController/SheetTransition.swift
@@ -0,0 +1,97 @@
+//
+//  SheetTransition.swift
+//  Quoti
+//
+//  Created by Eilon Krauthammer on 17/03/2021.
+//
+
+import UIKit
+
+final class SheetTransition: NSObject, UIViewControllerTransitioningDelegate, UIViewControllerAnimatedTransitioning{
+    
+    private var isPresent = false
+    
+    // MARK: - UIViewControllerTransitioningDelegate
+    
+    public func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        isPresent = true
+        return self
+    }
+    
+    public func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        isPresent = false
+        return self
+    }
+    
+    // MARK: - UIViewControllerAnimatedTransitioning
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.225
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        if isPresent {
+            animatePresentTransition(transitionContext: transitionContext)
+        } else {
+            animateDissmissalTransition(transitionContext: transitionContext)
+        }
+    }
+    
+    func animatePresentTransition(transitionContext: UIViewControllerContextTransitioning) {
+        
+        guard let from = transitionContext.viewController(forKey: .from) ,
+              let to = transitionContext.viewController(forKey: .to) as? SheetViewController  else {
+            return
+        }
+        
+        from.view.layoutIfNeeded()
+        to.view.layoutIfNeeded()
+        to.contentView.layoutIfNeeded()
+        to.contentContainerView.layoutIfNeeded()
+        to.view.backgroundColor = to.appearance.backgroundDimColor.withAlphaComponent(0.0)
+
+        let containerView = transitionContext.containerView
+        containerView.insertSubview(to.view, belowSubview: from.view)
+        to.view.backgroundColor = .clear
+        to.view.alpha = 1
+        
+        let height = to.contentContainerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height
+        to.contentContainerView.transform = .init(translationX: 0, y: height)
+
+        UIView.animate(withDuration: self.transitionDuration(using: transitionContext), animations: {
+            to.contentContainerView.transform = .identity
+            to.view.backgroundColor = to.appearance.backgroundDimColor.withAlphaComponent(to.appearance.backgroundDimLevel)
+            to.view.layoutIfNeeded()
+            from.view.layoutIfNeeded()
+        }, completion: {
+            finished in
+            transitionContext.completeTransition(true)
+        })
+
+    }
+    
+    func animateDissmissalTransition(transitionContext: UIViewControllerContextTransitioning) {
+        guard let from = transitionContext.viewController(forKey: .from) as? SheetViewController,
+            let to = transitionContext.viewController(forKey: .to) else {
+                return
+        }
+        
+        let containerView = from.contentContainerView
+        from.view.layoutIfNeeded()
+        to.view.layoutIfNeeded()
+        
+        UIView.animate(withDuration: self.transitionDuration(using: transitionContext), animations: {
+            from.view.backgroundColor = from.appearance.backgroundDimColor.withAlphaComponent(0)
+            from.view.backgroundColor = .clear
+            containerView.transform = .init(translationX: 0, y: containerView.frame.height)
+            from.view.layoutIfNeeded()
+            to.view.layoutIfNeeded()
+            
+            
+        }, completion: {
+            finished in
+            transitionContext.completeTransition(true)
+        })
+    }
+    
+}
---
+++ Sources/EKSwiftSuite/UI/SheetViewController/SheetViewController.swift
@@ -0,0 +1,109 @@
+//
+//  SheetViewController.swift
+//  Quoti
+//
+//  Created by Eilon Krauthammer on 17/03/2021.
+//
+
+import UIKit
+import EKSwiftSuite
+
+enum DismissState {
+    case closeButtonTap
+    case dimmedViewTap
+}
+
+protocol SheetViewControllerOutput: AnyObject {
+    func sheetViewController(_ controller: SheetViewController, wantsDismissAt dismissState: DismissState)
+}
+
+class SheetViewController: UIViewController {
+    struct Appearance {
+        let tintColor: UIColor
+        let backgroundColor: UIColor
+        let backgroundDimColor: UIColor
+        let backgroundDimLevel: CGFloat
+        let closeImage: UIImage
+    }
+
+    private let transition = SheetTransition()
+    
+    let appearance: Appearance
+    let contentContainerView: UIView = UIView()
+    let contentView: UIView
+    
+    weak var output: SheetViewControllerOutput?
+    
+    init(contentView: UIView, appearance: Appearance, output: SheetViewControllerOutput?) {
+        self.contentView = contentView
+        self.appearance = appearance
+        self.output = output
+        super.init(nibName: nil, bundle: nil)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        contentView = .init()
+        appearance = .init(tintColor: .systemBlue, backgroundColor: .white, backgroundDimColor: .white, backgroundDimLevel: 0.34, closeImage: .init())
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        transitioningDelegate = transition
+        modalPresentationStyle = .overFullScreen
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = appearance.backgroundDimColor.withAlphaComponent(appearance.backgroundDimLevel)
+        
+        configureContentView()
+        configureCloseButton()
+        configureDismissTapGesture()
+    }
+    
+    private func configureContentView() {
+        contentContainerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(contentContainerView)
+        NSLayoutConstraint.activate([
+            contentContainerView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            contentContainerView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            contentContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        contentView.fix(in: contentContainerView)
+    }
+    
+    private func configureCloseButton() {
+        let closeButton = SpringButton()
+        closeButton.setImage(appearance.closeImage, for: .normal)
+        closeButton.tintColor = appearance.tintColor
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.contentEdgeInsets = .even(4.0)
+        contentContainerView.addSubview(closeButton)
+        closeButton.topAnchor.constraint(equalTo: contentContainerView.topAnchor, constant: 12).isActive = true
+        closeButton.trailingAnchor.constraint(equalTo: contentContainerView.trailingAnchor, constant: -23).isActive = true
+        closeButton.addTarget(self, action: #selector(closeTapped), for: .touchUpInside)
+    }
+    
+    private func configureDismissTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dimmedViewTapped))
+        tapGesture.delegate = self
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func closeTapped() {
+        output?.sheetViewController(self, wantsDismissAt: .closeButtonTap)
+    }
+    
+    @objc private func dimmedViewTapped() {
+        output?.sheetViewController(self, wantsDismissAt: .dimmedViewTap)
+    }
+}
+
+extension SheetViewController: UIGestureRecognizerDelegate {
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        return gestureRecognizer.view === touch.view
+    }
+}
