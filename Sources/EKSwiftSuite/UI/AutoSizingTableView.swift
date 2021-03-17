50d42f5fbe61cadeae716240449b6ca09937467d
Eilon Krauthammer <eilon@socialkit.com>
2021-03-17 21:27:02 +0200

    openized once more

--- Sources/EKSwiftSuite/UI/AutoSizingTableView.swift
+++ Sources/EKSwiftSuite/UI/AutoSizingTableView.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 open class AutoSizingTableView: UITableView {
-    override func reloadData() {
+    open override func reloadData() {
         super.reloadData()
         self.invalidateIntrinsicContentSize()
         self.layoutIfNeeded()
     }
     
-    override var intrinsicContentSize: CGSize {
+    open override var intrinsicContentSize: CGSize {
         return CGSize(width: contentSize.width, height: contentSize.height)
     }
     
-    override func layoutSubviews() {
+    open override func layoutSubviews() {
         super.layoutSubviews()
         self.invalidateIntrinsicContentSize()
     }
     
-    override func awakeFromNib() {
+    open override func awakeFromNib() {
         super.awakeFromNib()
     }
 }
