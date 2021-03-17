cff6159d5d9a83f442f0519e6dc35982e0c2c3ff
Eilon Krauthammer <eilon@socialkit.com>
2021-03-17 14:50:32 +0200

    Added UIFont extension

---
+++ Sources/EKSwiftSuite/Extensions/UIFont+Extension.swift
@@ -0,0 +1,30 @@
+//
+//  UIFont+Extension.swift
+//  
+//
+//  Created by Eilon Krauthammer on 17/03/2021.
+//
+
+import Foundation
+
+public extension UIFont {
+    static func roundedFont(size: CGFloat, weight: UIFont.Weight) -> UIFont {
+        let baseFont = UIFont.systemFont(ofSize: size, weight: weight)
+        guard #available(iOS 13.0, *),
+              let fontDescriptor = baseFont.fontDescriptor.withDesign(.rounded) else {
+            return baseFont
+        }
+        let roundedFont = UIFont(descriptor: fontDescriptor, size: baseFont.pointSize)
+        return roundedFont
+    }
+    
+    static func fontWith(design: UIFontDescriptor.SystemDesign, size: CGFloat, weight: UIFont.Weight) -> UIFont {
+        let baseFont = UIFont.systemFont(ofSize: size, weight: weight)
+        guard #available(iOS 13.0, *),
+              let fontDescriptor = baseFont.fontDescriptor.withDesign(design) else {
+            return baseFont
+        }
+        let roundedFont = UIFont(descriptor: fontDescriptor, size: baseFont.pointSize)
+        return roundedFont
+    }
+}
