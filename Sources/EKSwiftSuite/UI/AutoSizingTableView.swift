840511d666b2e432c40d01fe56940f0104243bf9
Eilon Krauthammer <eilon@socialkit.com>
2021-03-17 21:25:14 +0200

    AutoSizingTableView

---
+++ Sources/EKSwiftSuite/UI/AutoSizingTableView.swift
@@ -0,0 +1,29 @@
+//
+//  AutoSizingTableView.swift
+//
+//
+//  Created by Eilon Krauthammer on 17/03/2021.
+//
+
+import UIKit
+
+class AutoSizingTableView: UITableView {
+    override func reloadData() {
+        super.reloadData()
+        self.invalidateIntrinsicContentSize()
+        self.layoutIfNeeded()
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: contentSize.width, height: contentSize.height)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.invalidateIntrinsicContentSize()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
