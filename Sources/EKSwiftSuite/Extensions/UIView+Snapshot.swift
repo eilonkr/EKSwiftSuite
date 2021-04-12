9171e022a51e55c1c39c17d6bd13b7550a7ed7e5
Eilon Krauthammer <eilon@socialkit.com>
2021-04-12 11:10:11 +0300

    Updated `makeSnapshot()`

--- Sources/EKSwiftSuite/Extensions/UIView+Snapshot.swift
+++ Sources/EKSwiftSuite/Extensions/UIView+Snapshot.swift
@@ -10,9 +10,9 @@ import UIKit
 public extension UIView {
     
     /// Use the preparation closure to configure the view before and after the snapshot was taken.
-    func makeSnapshot(using preparation: ((Bool) -> Void)?) -> UIImage {
+    func makeSnapshot(for subframe: CGRect? = nil, using preparation: ((Bool) -> Void)?) -> UIImage {
         preparation?(true)
-        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        let renderer = UIGraphicsImageRenderer(bounds: subframe ?? bounds)
         let snapshotImage = renderer.image { context in
             drawHierarchy(in: bounds, afterScreenUpdates: true)
         }
