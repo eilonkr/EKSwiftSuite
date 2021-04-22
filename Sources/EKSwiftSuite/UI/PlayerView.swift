89d000da28f019e5f270fb24e04f311e9c89c7cf
Eilon Krauthammer <eilon@socialkit.com>
2021-04-22 18:08:08 +0300

    Added `AVPlayer`

---
+++ Sources/EKSwiftSuite/UI/PlayerView.swift
@@ -0,0 +1,29 @@
+//
+//  PlayerView.swift
+//  
+//
+//  Created by Eilon Krauthammer on 22/04/2021.
+//
+
+import UIKit
+import AVFoundation
+
+open class PlayerView: UIView {
+    public var player: AVPlayer? {
+        get {
+            return playerLayer?.player
+        }
+        set {
+            playerLayer?.videoGravity = .resizeAspectFill
+            playerLayer?.player = newValue
+        }
+    }
+    
+    public weak var playerLayer: AVPlayerLayer? {
+        return layer as? AVPlayerLayer
+    }
+    
+    override public static var layerClass: AnyClass {
+        return AVPlayerLayer.self
+    }
+}
