aabb4d95591bc9d4e0ac7f5b5b4ce40708c687fc
Eilon Krauthammer <mitleyber@gmail.com>
2023-11-11 13:12:05 +0200

    Make `Gradient` decodable

--- Sources/EKSwiftSuite/UI/Gradient.swift
+++ Sources/EKSwiftSuite/UI/Gradient.swift
@@ -9,12 +9,23 @@
 
 import UIKit
 
-public struct Gradient: Equatable {
+public struct Gradient: Equatable, Decodable {
     public var direction: Direction
     public var colors: [UIColor]
     public var locations: [NSNumber]?
     
-    public init(direction: Gradient.Direction, colors: [UIColor], locations: [NSNumber]? = nil) {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        direction = try container.decode(Direction.self, forKey: .direction)
+        
+        let colorStrings = try container.decode([String].self, forKey: .colors)
+        colors = colorStrings.map { UIColor(hex: $0) }
+        
+        locations = nil
+    }
+    
+    public init(direction: Direction, colors: [UIColor], locations: [NSNumber]? = nil) {
         self.direction = direction
         self.colors = colors
         self.locations = locations
@@ -24,30 +35,54 @@ public struct Gradient: Equatable {
         Gradient(direction: .horizontal, colors: [color])
     }
     
-    public static func fade(direction: Gradient.Direction, startColor: UIColor) -> Gradient {
+    public static func fade(direction: Direction, startColor: UIColor) -> Gradient {
         Gradient(direction: direction, colors: [
             startColor,
             startColor.withAlphaComponent(0.0)
         ])
     }
+    
+    private enum CodingKeys: String, CodingKey {
+        case direction
+        case colors
+        case locations
+    }
 }
 
 public extension Gradient {
-    enum Direction: Equatable {
+    enum Direction: Equatable, Decodable {
         public static func == (lhs: Gradient.Direction, rhs: Gradient.Direction) -> Bool {
             switch (lhs, rhs) {
-                case (.vertical, .vertical):
-                    return true
-                case (.horizontal, .horizontal):
-                    return true
-                case (.diagonalLTR, .diagonalLTR):
-                    return true
-                case (diagonalRTL, .diagonalRTL):
-                    return true
-                case (.custom(let p1), .custom(let p2)):
-                    return p1 == p2
-                default:
-                    return false
+            case (.vertical, .vertical):
+                return true
+            case (.horizontal, .horizontal):
+                return true
+            case (.diagonalLTR, .diagonalLTR):
+                return true
+            case (.diagonalRTL, .diagonalRTL):
+                return true
+            case (.custom(let p1), .custom(let p2)):
+                return p1 == p2
+            default:
+                return false
+            }
+        }
+        
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.singleValueContainer()
+            let stringValue = try container.decode(String.self)
+            
+            switch stringValue {
+            case "vertical":
+                self = .vertical
+            case "horizontal":
+                self = .horizontal
+            case "diagonalLTR":
+                self = .diagonalLTR
+            case "diagonalRTL":
+                self = .diagonalRTL
+            default:
+                self = .horizontal
             }
         }
         
@@ -56,16 +91,16 @@ public extension Gradient {
         
         public var points: (start: CGPoint, end: CGPoint) {
             switch self {
-                case .vertical:
-                    return (CGPoint(x: 0.5, y: 0.0), CGPoint(x: 0.5, y: 1.0))
-                case .horizontal:
-                    return (CGPoint(x: 0.0, y: 0.5), CGPoint(x: 1.0, y: 0.5))
-                case .diagonalLTR:
-                    return (CGPoint(x: 0.0, y: 0.0), CGPoint(x: 1.0, y: 1.0))
-                case .diagonalRTL:
-                    return (CGPoint(x: 1.0, y: 0.0), CGPoint(x: 0.0, y: 1.0))
-                case .custom(let points):
-                    return (points.0, points.1)
+            case .vertical:
+                return (CGPoint(x: 0.5, y: 0.0), CGPoint(x: 0.5, y: 1.0))
+            case .horizontal:
+                return (CGPoint(x: 0.0, y: 0.5), CGPoint(x: 1.0, y: 0.5))
+            case .diagonalLTR:
+                return (CGPoint(x: 0.0, y: 0.0), CGPoint(x: 1.0, y: 1.0))
+            case .diagonalRTL:
+                return (CGPoint(x: 1.0, y: 0.0), CGPoint(x: 0.0, y: 1.0))
+            case .custom(let points):
+                return (points.0, points.1)
             }
         }
     }
